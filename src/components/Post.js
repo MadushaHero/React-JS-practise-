94e71bcb41f53bf56f7333302df2f809156dcbcd
MadushaHero <madushalakmal@gmail.com>
2022-01-17 14:44:50 +0530

    Dom Event

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,6 +9,21 @@ class Post extends Component  {
         "skills" :['React','Java script','HTML']
     }
 
+    handleClick=(e)=>{
+
+        console.log(this.state);
+    }
+
+    handleMouseOver=(e)=> {
+
+        console.log(e.target,e.pageX);
+    }
+
+    handleCopy =(e)=> {
+
+        console.log('Paragraph has been copy');
+    }
+
 render () {
 
     return (
@@ -21,6 +36,9 @@ render () {
                 <p> Name :- {this.state.Name}</p>
                 <p> Age :- {this.state.age}</p>
                 <p> Skills:- {this.state.skills.join(',')}</p>
+                <button onClick={this.handleClick}>click Here</button>
+                <button onMouseOver={this.handleMouseOver}>Mouse Over Here</button>
+                <p onCopy={this.handleCopy}>This Is Test Paragraph</p>
         </div>
     )
 }
