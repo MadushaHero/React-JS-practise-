7d77a2b3492a9e6d0e3606bb343b598658fff50a
MadushaHero <madushalakmal@gmail.com>
2022-01-18 11:53:29 +0530

    Stateless Components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Post'
 import Post from './components/Post';
+import Teacher from './components/Teacher';
 
 function App() {
   return (
     <div className="App">
       <Post />
+      <Teacher name= 'Madusha' email= 'madu@123.com' phone='01026354'/>
     </div>
   );
 }
---
+++ src/components/Teacher.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Teacher = (props) => {
+
+    const {name,email,phone}  = props;
+    return (
+        <div>
+            <h3>Teachers Details</h3>
+            <p> Name : {name}</p>
+            <p> Email : {email}</p>
+            <p> Phone : {phone}</p>
+        </div>
+    )
+}
+
+
+export default Teacher
